d0105c3dbcdfbdeaae1025654889b1f10990bfba
IT20648190 <it20648190@my.sliit.lk>
2023-09-25 13:48:27 +0530

    Changes in JS

--- js/homescript.js
+++ js/homescript.js
@@ -16,10 +16,22 @@ function showSlides(n) {
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        if (slides[i] instanceof HTMLElement) {
+            slides[i].style.display = "none";
+        } else {
+            console.error("Invalid DOM element at index 'i'");
+        }
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+        //dots[i].className = dots[i].className.replace(" active", "");
+        if (Array.isArray(dots) && Number.isInteger(i) && i >= 0 && i < dots.length) {
+            // Check if 'active' class exists in the element's class list before replacing it
+            if (dots[i].classList.contains("active")) {
+                dots[i].classList.remove("active");
+            }
+        } else {
+            console.error("Invalid input for 'dots' or 'i'");
+        }
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
